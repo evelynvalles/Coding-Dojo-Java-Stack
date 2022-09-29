41db0f7656cdfa028a11b0c1bde54774303986f4
Evelyn Valles <106193803+evelynvalles@users.noreply.github.com>
2022-09-28 19:29:21 -0500

    first commit

---
+++ Spring/HelloHuman/src/test/java/com/evelynvalles/hellohuman/HelloHumanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evelynvalles.hellohuman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloHumanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
